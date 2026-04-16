7c0d2bf0b1b8847dfab2576b85ea584d6eaffd5d
BIm0 Z <mouhibbouzamita@gmail.com>
2026-04-16 10:35:35 +0000

    feat compatibility with multiple terminals

--- deploy/build-hmd.sh
+++ deploy/build-hmd.sh
@@ -3,11 +3,10 @@
 set -e
 
 TAG="${1:-v15}"
-export APPS_JSON_BASE64=$(base64 -w 0 apps.json)
 
-docker build \
+DOCKER_BUILDKIT=1 docker build \
   --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
   --build-arg=FRAPPE_BRANCH=version-15 \
-  --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
+  --secret id=apps_json,src=apps.json \
   --tag=hmd-agro-prod:${TAG} \
   --file=images/layered/Containerfile .
